8e7bab538104a41dd9a09153aff6035f7d035792
roniklinger <roni.klinger@post.runi.ac.il>
2024-12-09 17:15:55 +0200

    check Scrabble 6

--- Scrabble.java
+++ Scrabble.java
@@ -145,7 +145,7 @@ public class Scrabble {
 
 					score = score + wordScore(input);
 					hand = MyString.remove(hand, input);
-					System.out.println(input + " earned " + wordScore(input) +" points. Score: " + score +" points");
+					System.out.println(input + " earned " + wordScore(input) +" points. Score: " + score +" points\n");
 				}
 				else{
 					System.out.println("No such word in the dictionary. Try again.");
